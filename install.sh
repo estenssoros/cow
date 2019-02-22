b2a854f71b421c041810eb8d0c50cb54ce7cfd69
estenssoros <data@seaspanltd.ca>
2019-02-21 22:38:49 -0700

    include all dirs

--- install.sh
+++ install.sh
@@ -4,6 +4,18 @@ go-bindata \
   -o initializer/bindata.go \
   -pkg initializer \
   -prefix templates \
-  templates templates/server templates/src
+  templates \
+  templates/public \
+  templates/server \
+  templates/server/api \
+  templates/src \
+  templates/src/actions \
+  templates/src/components \
+  templates/src/components/navigation \
+  templates/src/constants \
+  templates/src/modules \
+  templates/src/reducers \
+  templates/src/store \
+  templates/src/views
 
 go install
