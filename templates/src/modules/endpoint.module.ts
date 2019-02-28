3f9d4c3b2596a7e7f2a23f64ed3672d223baeebc
estenssoros <data@seaspanltd.ca>
2019-02-28 10:42:51 -0700

    only need login endpoint

--- templates/src/modules/endpoint.module.ts
+++ templates/src/modules/endpoint.module.ts
@@ -1,21 +1,3 @@
 export const URLS = {
-  login: 'api/login',
-  logout: 'api/logout',
-  config: 'api/config',
-  s3: 'api/s3',
-  local: 'api/local',
-  downloadS3: 'api/download-s3',
-  syncCDS: 'api/sync-cds',
-  deleteS3: 'api/delete-s3',
-  uploadLocal: 'api/upload-local',
-  openLocal: 'api/open-local',
-  formatLocal: 'api/format-local',
-  deleteLocal: 'api/delete-local',
-  copyLocal: 'api/copy-local',
-  syncLocalLocal: 'api/sync-local-local',
-  syncS3Local: 'api/sync-s3-local',
-  inspectFile: {
-    fileInfo: 'api/inspect-file/file-info',
-    sheetInfo: 'api/inspect-file/sheet-info'
-  }
+  login: 'api/login'
 }
